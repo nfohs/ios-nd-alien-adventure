98943d798b65b5047c02e3320f05a38346a1defa
nfohs <nick.fohs@gmail.com>
2016-10-02 23:26:08 -0700

    completed 'Items from Planet' to allow for checking of planet by name

--- Alien Adventure/ItemsFromPlanet.swift
+++ Alien Adventure/ItemsFromPlanet.swift
@@ -9,9 +9,25 @@
 extension Hero {
     
     func itemsFromPlanet(inventory: [UDItem], planet: String) -> [UDItem] {
-        return [UDItem]()
+        
+        var planetItemsNeeded = [UDItem]()
+        
+        //Check to make sure inventory isn't empty
+        if inventory.count != 0 {
+            //Evaluate each item in inventory for all the items from a specific planet
+            for item in inventory {
+                if let originPlanet = item.historicalData["PlanetOfOrigin"], originPlanet as? String == planet {
+                    print("Handing Over \(item.name) That's definitely from \(originPlanet)")
+                    planetItemsNeeded.append(item)
+                    print("we've handed over \(planetItemsNeeded.count)")
+                } else {
+                    print("\(item.name) is not from Glinda")
+                }
+            }
+            return planetItemsNeeded
+        }else {
+            print("Your inventory is empty")
+            return planetItemsNeeded
+        }
     }
-    
 }
-
-// If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
