05a616291465736b5b04c4ee3515eab31cc80073
nfohs <nick.fohs@gmail.com>
2016-10-08 16:04:44 -0700

    Working Code for 'Rarity of Items'

--- Alien Adventure/RarityOfItems.swift
+++ Alien Adventure/RarityOfItems.swift
@@ -9,7 +9,19 @@
 extension Hero {
     
     func rarityOfItems(inventory: [UDItem]) -> [UDItemRarity:Int] {
-        return [UDItemRarity:Int]()
+        //initialize the dictionary
+        var itemRarityDict = [UDItemRarity:Int]()
+        //set the base dictionary values to 0
+        itemRarityDict = [.common:0, .uncommon:0, .rare:0, .legendary:0]
+        //go over every item in inventory, increment the rarity count for each item in inventory
+        for item in inventory {
+            print("\(item.name) is a \(item.rarity) item")
+            itemRarityDict[item.rarity]? += 1
+            //disclosure - had help from http://stackoverflow.com/questions/33575585/increment-an-integer-in-a-swift-dictionary
+        }
+        print(itemRarityDict)
+        return itemRarityDict
+        
     }
 }
 
