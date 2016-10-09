68d538a651f95fce1713cd5cb7b57019eaa5c840
nfohs <nick.fohs@gmail.com>
2016-10-08 22:42:39 -0700

    Working code for PlanetData

--- Alien Adventure/PlanetData.swift
+++ Alien Adventure/PlanetData.swift
@@ -10,7 +10,35 @@ import Foundation
 extension Hero {
     
     func planetData(dataFile: String) -> String {
-        return ""
+        var planetRating = [String:Int]()
+        var mostIntriguingPlanet = String()
+        //load JSON file into dictionary
+        let planetJSON = Bundle.main.url(forResource: "PlanetData", withExtension: "json")!
+        let rawPlanetJSON = try! Data(contentsOf: planetJSON)
+        var planetDictionaryFromJSON: [[String:Any]]
+        do {
+            planetDictionaryFromJSON = try! JSONSerialization.jsonObject(with: rawPlanetJSON) as! [[String:Any]]
+        }
+        //evaluate each planet in the JSON file
+        for planet in planetDictionaryFromJSON {
+            var planetValue = Int()
+            //load all the values to generate intrigue score,update the planet rating dictionary with results
+            if let name = planet["Name"] as? String, let commonCount = planet["CommonItemsDetected"] as? Int, let uncommonCount = planet["UncommonItemsDetected"] as? Int, let rareCount = planet["RareItemsDetected"] as? Int, let legendaryCount = planet["LegendaryItemsDetected"] as? Int {
+                    planetValue = commonCount + uncommonCount * 2 + rareCount * 3 + legendaryCount * 4
+                    print("\(name) is valued at \(planetValue)")
+                    planetRating.updateValue(planetValue, forKey: name)
+                    print("\(planetRating)")
+                }
+            }
+        //check values in planetRating dictionary, set mostIntriguingPlanet
+        print("highest value should be \(planetRating.values.max())")
+        for (key, value) in planetRating {
+            if value == planetRating.values.max() {
+                mostIntriguingPlanet = key
+            }
+            //had help from http://stackoverflow.com/questions/38512265/how-to-find-the-key-with-the-min-max-value-in-a-swift-dictionary
+        }
+        return mostIntriguingPlanet
     }
 }
 
