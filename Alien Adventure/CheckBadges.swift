5579b9f79f7cbdc7e595eb2140af44c2ddb9247e
nfohs <nick.fohs@gmail.com>
2016-10-15 20:55:53 -0700

    working implementation of CheckBadges

--- Alien Adventure/CheckBadges.swift
+++ Alien Adventure/CheckBadges.swift
@@ -5,11 +5,40 @@
 //  Created by Jarrod Parkes on 10/4/15.
 //  Copyright © 2015 Udacity. All rights reserved.
 //
+//// Alien Adventure 3 checks:
+//case basicCheck = "BasicCheck"
+//case advancedCheck = "AdvancedCheck"
+//case expertCheck = "ExpertCheck"
+//case checkBadges = "CheckBadges"
+
 
 extension Hero {
     
+   
     func checkBadges(badges: [Badge], requestTypes: [UDRequestType]) -> Bool {
-        return true
+        var badgeCheckResult: Bool = false
+        var badgesObtained = [String]()
+        //add badge names to an array
+        for badge in badges {
+            badgesObtained.append(badge.requestType.rawValue)
+        }
+        print("We have earned \(badgesObtained)")
+        //cycle through all requestTypes the alien is looking for
+        if requestTypes != [] {
+            for request in requestTypes {
+                print("checking for \(request.rawValue)")
+                if badgesObtained.contains(request.rawValue) {
+                    print("it's a match - we have \(request.rawValue)")
+                    badgeCheckResult = true
+                } else {
+                    print("we don't have \(request.rawValue)")
+                    badgeCheckResult = false
+                }
+            }
+        } else {
+            print("he's not checking for any badges")
+            badgeCheckResult = true
+        }
+        return badgeCheckResult
     }
-    
 }
