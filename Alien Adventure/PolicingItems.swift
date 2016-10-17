a686cb05cfde2984231445bc7800c16877a61e61
nfohs <nick.fohs@gmail.com>
2016-10-17 15:16:42 -0700

    Working solution to report on number of errors thrown

--- Alien Adventure/PolicingItems.swift
+++ Alien Adventure/PolicingItems.swift
@@ -9,8 +9,31 @@
 extension Hero {
     
     func policingItems(inventory: [UDItem], policingFilter: (UDItem) throws -> Void) -> [UDPolicingError:Int] {
-        return [UDPolicingError:Int]()
-    }    
+        var policingErrors = [UDPolicingError.nameContainsLaser:0, UDPolicingError.itemFromCunia:0, UDPolicingError.valueLessThan10:0]
+        print("there are \(inventory.count) items in inventory")
+        for item in inventory {
+            do {
+                try policingFilter(item)
+            } catch UDPolicingError.nameContainsLaser {
+                let error = UDPolicingError.nameContainsLaser
+                print("\(item.name) has laser in the name")
+                policingErrors[error] = policingErrors[error]! + 1
+            } catch UDPolicingError.itemFromCunia {
+                let error = UDPolicingError.itemFromCunia
+                print("\(item.name) is from Cunia")
+                policingErrors[error] = policingErrors[error]! + 1
+            } catch UDPolicingError.valueLessThan10 {
+                let error = UDPolicingError.valueLessThan10
+                print("\(item.name) has value less than 10")
+                policingErrors[error] = policingErrors[error]! + 1
+            } catch {
+                print("another error")
+            }
+        }
+        print(policingErrors)
+        return policingErrors
+    }
 }
+
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
